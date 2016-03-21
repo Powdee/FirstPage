d7c5e4813260af9c35e01c85d015debd6556b3f7
Powdee <e.kurjak@gmail.com>
2016-03-21 19:04:04 +0000

    Modified envi

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,7 @@
 class ContactMailer < ActionMailer::Base
     default to: "e.kurjak@gmail.com"
     
-    def contact_name(name, email, body)
+    def initialize(name, email, body)
        @name = name
        @email = email 
        @body = body

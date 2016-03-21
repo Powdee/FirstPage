186e0cdcb606e280ee338f4578b119f9a7898f97
Powdee <e.kurjak@gmail.com>
2016-03-21 18:07:18 +0000

    Modified envi

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = { 
+    :address => 'smtp_sendrig.net',
+    :port => '587' ,
+    :authentication => :plain ,
+    :user_name => ENV['SENDRIG_USERNAME'] ,
+    :password => ENV['SENDRIG_PASSWORD'],
+    :domain => 'heroku.com' ,
+    :anable_starttls_auto => true
+}
